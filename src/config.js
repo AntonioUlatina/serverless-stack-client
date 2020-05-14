b50d67802dff67e083996042c7dd4280ab35ae90
unknown <antonio.alvarez1@ulatina.net>
2020-05-13 21:07:49 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ca-central-1",
+    BUCKET: "notes-tutorial-bucket",
+  },
+  apiGateway: {
+    REGION: "ca-central-1",
+    URL: "https://ezqe3pto4h.execute-api.ca-central-1.amazonaws.com/prod/",
+  },
+  cognito: {
+    REGION: "ca-central-1",
+    USER_POOL_ID: "ca-central-1_fMfZgeVL5",
+    APP_CLIENT_ID: "t8anolveub95v79bh0eqvfhqh",
+    IDENTITY_POOL_ID: "ca-central-1:76cf78a8-4309-44bb-a484-d07b958060af",
+  },
+};
